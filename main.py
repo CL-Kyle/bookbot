6cca701fb48bc88302c472af4cabb9048d32d255
CL-Kyle <artykrav@gmail.com>
2025-06-03 23:29:01 -0400

    Added get_book_char func

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_book_num
+from stats import get_book_char
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,4 +9,6 @@ def get_book_text(path_to_file):
 def main():
     num_words = get_book_num(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+    dict_var = get_book_char(get_book_text("books/frankenstein.txt"))
+    print(dict_var)
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def get_book_num(contents):
     list_contents = contents.split()
     list_count = len(list_contents)
     return list_count
+
+def get_book_char(contents):
+    contents_lower = contents.lower()
+    char_dict = {}
+    for char in contents_lower:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+    return char_dict
