4c13b2098272709c224ae7754d47717edd8fc668
CL-Kyle <artykrav@gmail.com>
2025-06-03 22:19:16 -0400

    Created counting of words func

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ def get_book_text(path_to_file):
         contents = f.read()
     return contents
 
+def get_book_num(contents):
+    list_contents = contents.split()
+    list_count = len(list_contents)
+    return list_count
+
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    num_words = get_book_num(get_book_text("books/frankenstein.txt"))
+    print(f"{num_words} words found in the document")
 main()
