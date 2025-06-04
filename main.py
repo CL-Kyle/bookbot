0b9529883961611df174a99cdda5a8a680a3cd0d
CL-Kyle <artykrav@gmail.com>
2025-06-03 22:25:26 -0400

    Refactored with stats.py

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
+from stats import get_book_num
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         contents = f.read()
     return contents
-
-def get_book_num(contents):
-    list_contents = contents.split()
-    list_count = len(list_contents)
-    return list_count
-
 
 def main():
     num_words = get_book_num(get_book_text("books/frankenstein.txt"))
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_book_num(contents):
+    list_contents = contents.split()
+    list_count = len(list_contents)
+    return list_count
