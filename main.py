e4d25d6dcf5d2a7696aba63f6ba378894a81ae65
CL-Kyle <artykrav@gmail.com>
2025-06-07 18:54:58 -0400

    added func get_book_char, dict_list, sort_on, alpha_list. Prints a report when ran.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from stats import get_book_num
 from stats import get_book_char
+from stats import sort_on
+from stats import dict_list
+from stats import alpha_list
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,7 +11,22 @@ def get_book_text(path_to_file):
 
 def main():
     num_words = get_book_num(get_book_text("books/frankenstein.txt"))
-    print(f"{num_words} words found in the document")
+    # print(f"{num_words} words found in the document")
     dict_var = get_book_char(get_book_text("books/frankenstein.txt"))
-    print(dict_var)
+    # print(dict_var)
+    char_dict_list = dict_list(dict_var)
+    char_dict_list.sort(reverse=True, key=sort_on)
+    # print(char_dict_list)
+    # print(alpha_list(char_dict_list))
+    alpha_list_prime = alpha_list(char_dict_list)
+
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for i in alpha_list_prime:
+        print(f"{i['char']}: {i['num']}")
+    print("============= END ===============")
+    
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,21 @@ def get_book_char(contents):
         else:
             char_dict[char] += 1
     return char_dict
+
+def dict_list(char_dict):
+    char_dict_list = []
+    for char in char_dict:
+        num = char_dict[char]
+        small_dict = {"char": char, "num": num}
+        char_dict_list.append(small_dict)
+    return char_dict_list
+
+def sort_on(dict):
+    return dict["num"]
+
+def alpha_list(char_dict_list):
+    alpha_list =[]
+    for char in char_dict_list:
+        if char["char"].isalpha() and char["char"] in "abcdefghijklmnopqrstuvwxyz":
+            alpha_list.append(char)
+    return alpha_list
